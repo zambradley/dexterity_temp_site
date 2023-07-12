36960b6ef20311319d66d4e8492de38fc8f46451
Bradley Zambrano <zambradley3@gmail.com>
2023-07-12 11:26:48 -0500

    version 1.0 - requirements folder added

--- dexterity_temp/settings.py
+++ dexterity_temp/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_(f10e^3$kuy2$ztl&czrvs#4jf-@cxg6oqiog$4_mgzm2$@x0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
